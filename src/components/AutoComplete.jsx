27570d365dae05f7016e93ccfae2e374e09bc99b
Adir <adir2293@gmail.com>
2022-09-10 21:45:55 +0300

    Auto Complete component setup finished

--- src/components/AutoComplete.jsx
+++ src/components/AutoComplete.jsx
@@ -1,3 +1,61 @@
+import { useState, useEffect } from "react"
+import finnHub from "../apis/finnHub"
+
 export const AutoComplete = () => {
-    return <div>AutoComplete</div>
+    const [search, setSearch] = useState("")
+    const [results, setResults] = useState([])
+
+    const renderDropdown = () => {
+        const dropDownClass = search ? "show" : null
+        return (
+            <ul style={{
+                height: "300px",
+                overflowY: "scroll",
+                overflowX: "hidden",
+                cursor: "pointer"
+            }}className={`dropdown-menu ${dropDownClass}`}>
+                {results.map((result) => {
+                    return (
+                        <li className="dropdown-item" key={result.symbol}>
+                            {result.description} ({result.symbol})
+                        </li>
+                    )
+                })}
+            </ul>
+        )
+    }
+
+    useEffect(() => {
+        let isMounted = true
+        const fetchData = async () => {
+            try {
+                const response = await finnHub.get("/search", {
+                    params: {
+                        q: search
+                    }
+                })
+                
+                if (isMounted) {
+                    setResults(response.data.result)
+                }
+
+            } catch (err) {
+
+            }
+        }
+        if (search.length > 0) {
+            fetchData()
+        } else {
+            setResults([])
+        }
+    }, [search])
+
+    return <div className="w-50 p-5 rounded mx-auto">
+        <div className="form-floating dropdown">
+            <input style={{ backgroundColor: "rgba(145, 158, 171, 0.04)" }} id="search" type="text" className="form-control" placeholder="Search" autoComplete="off" 
+            value={search} onChange={(e) => setSearch(e.target.value)}></input>
+            <label htmlFor="search">Search for a stock symbol</label>
+            {renderDropdown()}
+        </div>
+    </div>
 }
