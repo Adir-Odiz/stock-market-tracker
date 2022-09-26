9f1f6c57794e7e0e2e1db918e9d65326be646ee1
Adir <adir2293@gmail.com>
2022-09-26 04:37:49 +0300

    sdp

--- src/pages/StockDetailPage/StockDetailPage.jsx
+++ src/pages/StockDetailPage/StockDetailPage.jsx
@@ -1,3 +1,14 @@
+import { useEffect } from "react"
+import { useParams } from "react-router-dom"
+import finnHub from "../../apis/finnHub"
+
 export const StockDetailPage = () => {
-    return <div>Stock Detail Page</div>
+    const {symbol} = useParams()
+    useEffect(() => {
+        const fetchData = async () => {
+            
+        }
+    })
+
+    return <div>{symbol}</div>
 }
